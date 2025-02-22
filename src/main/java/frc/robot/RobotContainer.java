2246c22a07b4e3b3c78dc225455e8860b3c68a26
Noah Smith <smithn@mepoedu.org>
2025-02-22 09:08:43 -0600

    Remove Test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -115,8 +115,6 @@ public class RobotContainer {
       m_gameCommands.changePivotPointCommand(.1)
     );
 
-    //extension test
-
     operator.povUp().whileTrue(
       m_gameCommands.elevatorUpCommand(1)
     );
